b6143db078bf242d171af25cdf9f04e4e245a870
mike24021012 <114869100+mike24021012@users.noreply.github.com>
2023-09-07 15:02:02 +0800

    Update lcd1602_test.c

--- lcd1602_test.c
+++ lcd1602_test.c
@@ -13,29 +13,50 @@
 
 int main(void)
 {
-	int res,lcd1602_file;
-
+	int res,lcd1602_file,i;
+	char my_command,my_buf[50],sentence[33];
+	
 	lcd1602_file = open(DEVICE_NAME, O_RDWR);
 	if(lcd1602_file < 0)
 	{
-		perror("can not open sg90 device");
+		perror("can not open lcd1602 device");
 		exit(1);
 	}
-	
+	printf("open lcd1602 device success.\n");
+
 	while(1)
 	{
-	    printf("Input command, 1:run positive 2:run negative 3:stop 4:leave program.\n");
-		scanf("%c",&mycmd);
+		printf("Input command, 1:write sentences to lcd1602, 2:leave program.\n");
+		scanf("%c",&my_command);
 		getchar();
-		switch(mycmd)
-	
-	
+		switch(my_command)
+		{
+		case '1':
+			printf("please input your sentences(max length: 32 characters):\n");
+			scanf("%[^\n]s",my_buf);
+			getchar();
+			for(i=0;i<=31;++i)
+				sentence[i]=my_buf[i];
+			my_buf[32]='\0';
+			res = write(lcd1602_file,my_buf,sizeof(my_buf));
+    		if(res < 0)
+    		{
+    			printf("write err!\n");
+			}
+			continue;
+		case '2':
+			break;
+		default:
+			printf("Wrong command,please input again.\n");
+			continue;
+		}
+		break;		
 	}
 	
 	if (lcd1602_file >= 0)	 //close humidityfd if open
 	{
 		close(lcd1602_file);
-		printf("close file success.\n");
+		printf("close lcd1602 device success.\n");
 	}
 	
 	return 0;
